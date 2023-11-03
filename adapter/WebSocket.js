80b4ed32f3c6fab5527c229f5efa754bc4527637
Zyy955 <1072411694@qq.com>
2023-11-03 21:05:59 +0800

    端口占用后尝试解除

--- adapter/WebSocket.js
+++ adapter/WebSocket.js
@@ -3,6 +3,7 @@ import ComWeChat from "./WeChat/ComWx.js"
 import express from "express"
 import { createServer } from "http"
 import common from "../model/common.js"
+import fetch from "node-fetch"
 
 export default class WebSocket {
     constructor() {
@@ -19,10 +20,29 @@ export default class WebSocket {
         /** 创建Express应用程序 */
         const app = express()
         /** 创建HTTP服务器 */
-        const httpServer = createServer(app)
+        this.Server = createServer(app)
+
+        /** 解除端口占用api */
+        app.get("/api/close-server", async (req, res) => {
+            const time = new Date(Date.now()).toISOString()
+            const ip = req.ip
+            if (ip !== "::1" && ip !== "127.0.0.1") {
+                return res.status(401).json({ error: "未经授权的访问" })
+            }
+
+            try {
+                res.json({ message: "正在关闭当前服务器..." })
+                this.Server.close(() => {
+                    logger.warn(`[Lain-plugin] 服务器在另外一处启动，正在关闭当前服务器...`)
+                })
+            } catch (error) {
+                logger.error(error)
+                res.status(500).json({ error: "关闭服务器时出错" })
+            }
+        })
 
         /** 将WebSocket服务器实例与HTTP服务器关联 */
-        httpServer.on("upgrade", (request, socket, head) => {
+        this.Server.on("upgrade", (request, socket, head) => {
             const pathname = request.url
             if (pathname === this.path) {
                 shamrock.handleUpgrade(request, socket, head, (socket) => {
@@ -40,9 +60,31 @@ export default class WebSocket {
             }
         })
 
-        httpServer.listen(this.port, async () => {
+        this.Server.listen(this.port, async () => {
             await common.log("", `本地 Shamrock 连接地址：${logger.blue(`ws://localhost:${this.port}${this.path}`)}`)
             await common.log("", `本地 ComWeChat 连接地址：${logger.blue(`ws://localhost:${this.port}${this.path_wx}`)}`)
+        })
+
+        /** 捕获错误 */
+        this.Server.on("error", async (error) => {
+            if (error.code === "EADDRINUSE") {
+                logger.error(`[Lain-plugin] 端口${this.port}已被占用，正在尝试解除`)
+                try {
+                    const response = await fetch(`http://localhost:${this.port}/api/close-server`)
+                    if (response.ok) {
+                        await common.sleep(5000)
+                        await common.log("", `本地 Shamrock 连接地址：${logger.blue(`ws://localhost:${this.port}${this.path}`)}`)
+                        await common.log("", `本地 ComWeChat 连接地址：${logger.blue(`ws://localhost:${this.port}${this.path_wx}`)}`)
+                        return
+                    } else {
+                        throw new Error(`HTTP请求失败，状态码: ${response.status}`)
+                    }
+                } catch (error) {
+                    logger.error(error)
+                }
+            } else {
+                logger.error(error)
+            }
         })
     }
 }
