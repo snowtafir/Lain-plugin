4aac9887390c9779de9f011b35982ab88b0dd980
Zyy955 <1072411694@qq.com>
2023-11-04 19:48:05 +0800

    什么玩意

--- adapter/shamrock/message.js
+++ adapter/shamrock/message.js
@@ -102,7 +102,7 @@ export default new class zaiMsg {
                     }
                     return {
                         member,
-                        getAvatarUrl: () => `https://q1.qlogo.cn/g?b=qq&s=0&nk=${user_id}`
+                        getAvatarUrl: (userId = user_id) => `https://q1.qlogo.cn/g?b=qq&s=0&nk=${userId}`
                     }
                 },
                 getChatHistory: async (msg_id, num) => {
